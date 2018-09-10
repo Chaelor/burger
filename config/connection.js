84bb50ebec8d7bd3dafdc7645f2f8bb66c4e3088
Chaelor <joshsblizzard@gmail.com>
2018-09-10 12:29:27 -0500

    Views, models, controllers done.

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    user: "root",
+    password: "password",
     database: "burgers_db"
 });
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,44 @@
 //dependencies
-
 const express = require('express');
-const burgers = require('../models/burgers');
+const burger = require('../models/burgers');
+const router = express.Router();
+
+//Get root, display all things in the db
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+//Post a new (probably yucky beef) burger
+router.post("/api/burgers", function(req, res){
+    burger.createOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(res){
+        res.json({id: res.insertId});
+    });
+});
+
+//Update a devoured state of a burger
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = ` id = ${req.params.id}`;
+
+    console.log("condition: " + condition);
+
+    burger.updateOne(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function(result){
+            if(result.changedRows === 0){
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+module.exports = router;
